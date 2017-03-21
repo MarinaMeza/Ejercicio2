33887b2c5cb86ccc71e64ba867a3c72cb16cb62a
alumno <alumno>
2017-03-20 21:29:08 -0300

    Tercer commit ([0])

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //echo "hola mundo";
-
+/*
 $array=array
 (
     rand(),
@@ -8,8 +8,14 @@ $array=array
     rand(),
     rand(),
     rand(),
-);
+);*/
 
+$array=array();
+
+for($i=0;$i<5;$i++)
+{
+    $array[$i]=rand(1,10);
+}
 var_dump($array);
 
 
