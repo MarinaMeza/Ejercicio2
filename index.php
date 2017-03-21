f7d51caeafa1aacb051726bee0afd7b1a41395a6
alumno <alumno>
2017-03-20 21:49:38 -0300

    Quinto commit (ejercicio terminado)

--- index.php
+++ index.php
@@ -17,14 +17,43 @@ for($i=0;$i<5;$i++)
 }
 
 */
-
+/*
+Mediante una estructura condicional, determinar si el promedio de los números
+son mayores, menores o iguales que 6. Mostrar un mensaje por pantalla informando el
+resultado
+*/
 $array=array();
+$acumulador=0;
 
 for($i=0;$i<5;$i++)
 {
     array_push($array,rand(1,10));
 }
 
+for($i=0;$i<5;$i++)
+{
+    $acumulador+=$array[$i];
+}
+
+$acumulador=$acumulador/5;
+
 var_dump($array);
+
+echo "<br>Promedio: ".$acumulador."<br>";
+
+if($acumulador>6)
+{
+    echo "El promedio de los numeros es mayor a 6 ";
+}
+
+else if($acumulador==6)
+{
+    echo "El promedio de los numeros es igual a 6 ";
+}
+
+else
+{
+    echo "El promedio de los numeros es menor a 6 ";
+}
 
 ?>
